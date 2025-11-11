ee9d84657bf6b23660a3b008cb8ad0759e34d164
YOUR NAME <filippmiller@gmail.com>
2025-11-12 01:29:36 +0300

    cursor: fix dashboard refresh handlers

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -62,8 +62,8 @@ export default async function DashboardPage() {
       <ProfileForm initialProfile={profile} />
 
       <div className="grid gap-6 md:grid-cols-2">
-        <NewEntryForm onSuccess={() => window.location.reload()} />
-        <AudioRecorder onSuccess={() => window.location.reload()} />
+        <NewEntryForm onSuccess={() => {}} />
+        <AudioRecorder onSuccess={() => {}} />
       </div>
 
       <JournalEntriesList initialEntries={entries} initialNextCursor={nextCursor} />
